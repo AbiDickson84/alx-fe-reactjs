bf6e817e4fa0900f4b8f4dffdc175e60ab11d809
AbiDickson84 <dicksonjamesabi@gmail.com>
2025-07-20 13:19:56 -0500

    Create a Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-function Counter () {
+function Counter() {
   const [count, setCount] = useState(0);
-  return(
-    <div>
-      <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count -1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-     </div>
-);
+
+  return (
+    <div style={{ textAlign: 'center', margin: '20px' }}>
+      <p style={{ fontSize: '20px', fontWeight: 'bold' }}>Current Count: {count}</p>
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
