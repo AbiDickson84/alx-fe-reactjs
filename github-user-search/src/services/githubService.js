a9e7c2af60e5856524f6557c3518bf13debf54fe
AbiDickson84 <dicksonjamesabi@gmail.com>
2025-08-03 07:52:59 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+// Task 1: Basic search
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data; // single user object
+};
+
+// Task 2: Advanced search
 export const fetchAdvancedUsers = async (username, location, minRepos) => {
   let query = "";
   if (username) query += `${username}`;
