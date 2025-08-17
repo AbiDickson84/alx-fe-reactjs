c7a7e9a4f4788231df2c808037192449546cb53a
AbiDickson84 <dicksonjamesabi@gmail.com>
2025-08-17 17:39:31 +0100

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,15 +1,11 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import UserProfile from './components/UserProfile';
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen flex items-center justify-center bg-white">
       <UserProfile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
